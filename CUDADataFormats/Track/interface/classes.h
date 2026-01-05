07bda3ce682165e1dbb568215890b830976d480f
akritkbehera <akrit.kumar.behera@cern.ch>
2026-01-05 14:37:20 +0100

    classes.h and classes_def.xml for CUDADataFormats/Track

---
+++ CUDADataFormats/Track/interface/classes.h
@@ -0,0 +1,3 @@
+#include "CUDADataFormats/Track/interface/TrackSoAHeterogeneousHost.h"
+#include "CUDADataFormats/Track/interface/TrackingRecHitSoAHost.h"
+#include "CUDADataFormats/Track/interface/ZVertexSoAHeterogeneousHost.h"
